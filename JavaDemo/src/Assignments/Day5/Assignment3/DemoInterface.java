5e19cadddc0148d6ddf62ac35e5dd36c768a7ada
Amar <shreamar@isu.edu>
2021-06-15 23:30:14 -0500

    Day5: Assignment3 completed

---
+++ JavaDemo/src/Assignments/Day5/Assignment3/DemoInterface.java
@@ -0,0 +1,9 @@
+package Assignments.Day5.Assignment3;
+
+class DemoInterface {
+    public static void main(String[] args) {
+        Example example = new Example();
+        example.methodOne();
+        example.methodTwo();
+    }
+}
