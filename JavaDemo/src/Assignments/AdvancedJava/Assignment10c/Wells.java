c66e86ee9985cbe2f155a8e3686be1112ae5b0d4
Amar <shreamar@isu.edu>
2021-06-16 23:08:48 -0500

    Advanced Java: Assignment10c

---
+++ JavaDemo/src/Assignments/AdvancedJava/Assignment10c/Wells.java
@@ -0,0 +1,12 @@
+package Assignments.AdvancedJava.Assignment10c;
+
+public class Wells {
+    public static void main(String[] args) {
+        //EmployeeGrade floch = new EmployeeGrade();
+        EmployeeGrade hange = new EmployeeGrade(1103, "Hange Zoe", 4.5f, 4.6f,4.7f);
+
+        hange.calculateAverageFeedback();
+        hange.calculateGrade();
+        hange.displayInfo();
+    }
+}
