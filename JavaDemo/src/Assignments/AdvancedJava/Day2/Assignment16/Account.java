35d0c965b6a5229fbf0e09d6643423396641e7df
Amar <shreamar@isu.edu>
2021-06-17 21:47:45 -0500

    Advanced Java: Day5: Assignment16

---
+++ JavaDemo/src/Assignments/AdvancedJava/Day2/Assignment16/Account.java
@@ -0,0 +1,32 @@
+package Assignments.AdvancedJava.Day2.Assignment16;
+
+public abstract class Account {
+    private int accountNo;
+    private Customer customer;
+    protected double balance;
+
+    public Account(){
+        System.out.println("Account class constructor");
+    }
+    public Account(int accountNo, Customer customer, double balance){
+        System.out.println("Account class parameterized constructor");
+        this.accountNo = accountNo;
+        this.customer = customer;
+        this.balance = balance;
+    }
+
+    public Customer getCustomer(){
+        return customer;
+    }
+
+    public double balanceEnquiry(){
+        return balance;
+    }
+
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+
+    public abstract void withdraw(double amount);
+}
+
