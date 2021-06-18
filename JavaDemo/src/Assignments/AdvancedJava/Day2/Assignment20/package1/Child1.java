6fa941b987726dca9937ae1fd6a65430c91275b2
Amar <shreamar@isu.edu>
2021-06-17 22:26:46 -0500

    Advanced Java: Day2: Assignment20 completed

---
+++ JavaDemo/src/Assignments/AdvancedJava/Day2/Assignment20/package1/Child1.java
@@ -0,0 +1,10 @@
+package Assignments.AdvancedJava.Day2.Assignment20.package1;
+
+public class Child1 extends Base {
+    public void getValues(){
+        System.out.println(getVariable1());
+        System.out.println(getVariable2());
+        System.out.println(variable3);
+        System.out.println(variable4);
+    }
+}
---
+++ JavaDemo/src/Assignments/AdvancedJava/Day2/Assignment20/package2/Child2.java
@@ -0,0 +1,12 @@
+package Assignments.AdvancedJava.Day2.Assignment20.package2;
+
+import Assignments.AdvancedJava.Day2.Assignment20.package1.Base;
+
+public class Child2 extends Base {
+    public void getValues(){
+        Base base = new Base();
+        System.out.println(variable3);
+        System.out.println(variable3);
+        System.out.println(base.variable4);
+    }
+}
