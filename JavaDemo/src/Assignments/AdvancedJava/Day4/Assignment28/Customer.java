bab3be50331e6cdd4ba6c759b02de4408abcfb4d
Amar <shreamar@isu.edu>
2021-06-19 15:08:49 -0500

    Advanced Java: Day4: Assignemnt28 completed

---
+++ JavaDemo/src/Assignments/AdvancedJava/Day4/Assignment28/Customer.java
@@ -0,0 +1,40 @@
+package Assignments.AdvancedJava.Day4.Assignment28;
+
+public class Customer {
+    private int customerID;
+    private String customerName;
+    private String customerAddress;
+    private int pinCode;
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+}
---
+++ JavaDemo/src/Assignments/AdvancedJava/Day4/Assignment28/WellsBank.java
@@ -0,0 +1,16 @@
+package Assignments.AdvancedJava.Day4.Assignment28;
+
+public class WellsBank {
+    public static void main(String[] args) {
+        Customer jean = new Customer();
+        jean.setCustomerID(1006);
+        jean.setCustomerName("Jean Kirstein");
+        jean.setCustomerAddress("Scout Regiment HQ, Trost district");
+        jean.setPinCode(1234);
+
+        System.out.println("Customer ID: "+jean.getCustomerID());
+        System.out.println("Customer name: "+jean.getCustomerName());
+        System.out.println("Address: "+jean.getCustomerAddress());
+        System.out.println("Pin code: "+jean.getPinCode());
+    }
+}
